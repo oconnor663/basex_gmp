5f46fba9b8ebbf1cdb28974c3f628057c930c801
Jack O'Connor <oconnor663@gmail.com>
2016-01-08 09:11:28 -0500

    tests working

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 base62: base62.c
 	gcc base62.c -o base62 -lgmp -lm -O3 -Wall -pedantic
+
+test: base62
+	./test.sh
--- test.sh
+++ test.sh
@@ -2,6 +2,16 @@
 
 set -e -u -o pipefail
 
-for i in vectors/*.in ; do
-  echo $i
+tempfile="$(mktemp)"
+
+for infile in vectors/*.in ; do
+  outfile="${infile%.in}.out"
+  ./base62 < "$infile" > "$tempfile"
+  cmp "$outfile" "$tempfile"
+done
+
+for outfile in vectors/*.out ; do
+  infile="${outfile%.out}.in"
+  ./base62 -d < "$outfile" > "$tempfile"
+  cmp "$infile" "$tempfile"
 done
