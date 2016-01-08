ccb3c6710d4f8673f30ea3708b93b059ca41a09f
Jack O'Connor <oconnor663@gmail.com>
2016-01-08 02:38:11 -0500

    works -- was initializing LOG2_62 in the wrong place

--- base62.c
+++ base62.c
@@ -4,25 +4,6 @@
 #include <math.h>
 #include <stdbool.h>
 
-// int main() {
-//   char s[100];
-//   mpz_t x;
-//   mpz_init_set_ui(x, 3843);
-//   mpz_get_str(s, 62, x);
-//   printf("%s\n", s);
-//   mpz_add_ui(x, x, 1);
-//   mpz_get_str(s, 62, x);
-//   printf("%s\n", s);
-//   size_t count;
-//   memset(s, 0, 100);
-//   mpz_export(s, &count, 1 /*big*/, 1 /*1 byte words*/, 1 /*big*/, 0 /*nails*/, x);
-//   for (int i=0; i<count; i++) {
-//     printf("%d ", s[i]);
-//   }
-//   printf("\n");
-//   mpz_clear(x);
-// }
-
 float LOG2_62;
 
 size_t min_chars_for_bytes(size_t num_bytes) {
@@ -45,7 +26,6 @@ void print_chars(mpz_t* bignum, char* bytes_buffer, size_t bytes_count) {
 }
 
 void encode() {
-  LOG2_62 = log2f(62);  // initialize the log_2(A)
   char buffer[32];
   size_t count;
   mpz_t bignum;
@@ -100,8 +80,9 @@ void decode() {
       break;
     }
     for (int i=0; i<buffer_count; i++) {
-      if (is_base62_char(buffer[i])) {
-        input_block[block_index] = buffer[i];
+      char c = buffer[i];
+      if (is_base62_char(c)) {
+        input_block[block_index] = c;
         block_index = (block_index + 1) % 43;
         // If we just filled the block, serialize it.
         if (block_index == 0) {
@@ -118,6 +99,7 @@ void decode() {
 }
 
 int main(int argc, char** argv) {
+  LOG2_62 = log2f(62);  // initialize the log_2(A)
   if (argc >= 2 && strcmp(argv[1], "-d") == 0) {
     decode();
   } else {
